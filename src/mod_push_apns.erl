e7b015a78c16b48a3283131a8599002ef0912392
Christian Ulrich <christian@rechenwerk.net>
2015-08-21 04:46:50 +0200

    add retry mechanism and error handling to mod_push_apns
    The APNS backend now implements error handling and retrying. An error is
    recoverable if APNS responds with status code 10 (Shutdown) or 7
    (invalid payload size). The retry period is set by ?RETRY_INTERVAL, the
    time the backend waits for an error response is set by
    ?PENDING_INTERVAL. We will try to send multiple notifications at once,
    the maximum number is set by ?MAX_PENDING_NOTIFICATIONS.
    The ciphersuites had to be changed as Apple apparently changed their TLS
    configuration. This needs more investigation.

--- src/mod_push_apns.erl
+++ src/mod_push_apns.erl
@@ -41,41 +41,216 @@
 %-define(PUSH_URL, "gateway.push.apple.com").
 -define(PUSH_URL, "gateway.sandbox.push.apple.com").
 -define(APNS_PORT, 2195).
--define(SSL_TIMEOUT, 10000).
+-define(SSL_TIMEOUT, 3000).
+-define(MAX_PAYLOAD_SIZE, 2048).
 -define(MESSAGE_EXPIRY_TIME, 86400).
 -define(MESSAGE_PRIORITY, 10).
+-define(MAX_PENDING_NOTIFICATIONS, 16).
+-define(PENDING_INTERVAL, 3000).
+-define(RETRY_INTERVAL, 30000).
 -define(CIPHERSUITES,
         [{K,C,H} || {K,C,H} <- ssl:cipher_suites(erlang),
-                    K =/= ecdh_ecdsa, K =/= ecdh_rsa, K =/= rsa,
+                    %K =/= ecdh_ecdsa, K =/= ecdh_rsa, K =/= rsa,
+                    K =/= ecdh_ecdsa, K =/= ecdh_rsa,
                     C =/= rc4_128, C =/= des_cbc, C =/= '3des_ede_cbc',
-                    H =/= sha, H =/= md5]).
+                    %H =/= sha, H =/= md5]).
+                    H =/= md5]).
 
-% TODO: add message_queue
 -record(state,
         {certfile :: binary(),
-         out_socket :: ssl:socket()}).
+         out_socket :: ssl:socket(),
+         pending_list :: [{pos_integer(), any()}],
+         send_list :: [any()],
+         retry_list :: [any()],
+         pending_timer :: reference(),
+         retry_timer :: reference(),
+         pending_timestamp :: erlang:timestamp(),
+         message_id :: pos_integer()}).
 
 %-------------------------------------------------------------------------
 
 init([_AuthKey, _PackageSid, CertFile]) ->
-    ?DEBUG("+++++++++ mod_push_apns:init", []),
-    %inets:start(),
+    ?DEBUG("+++++++++ mod_push_apns:init, certfile = ~p", [CertFile]),
+    inets:start(),
     crypto:start(),
     ssl:start(),
-    {ok, #state{certfile = CertFile}}.
+    {ok, #state{certfile = CertFile,
+                pending_list = [],
+                send_list = [],
+                retry_list = [],
+                pending_timer = make_ref(),
+                retry_timer = make_ref(),
+                message_id = 0}}.
 
 %-------------------------------------------------------------------------
 
-handle_info({ssl, Socket, Data}, State) ->
-    <<Command:8, Status:8, Id:32/big>> = iolist_to_binary(Data),
-    ?INFO_MSG("received error reply from APNS: ~p / ~p / ~p",
-              [Command, Status, Id]),
-    ssl:close(Socket),
-    {noreply, State#state{out_socket = undefined}};
+%% error_received
+handle_info({ssl, _Socket, Data},
+            #state{pending_list = PendingList,
+                   retry_list = RetryList,
+                   pending_timer = PendingTimer,
+                   retry_timer = RetryTimer} = State) ->
+    erlang:cancel_timer(PendingTimer),
+    NewState =
+    try
+        <<RCommand:1/unit:8, RStatus:1/unit:8, RId:4/unit:8>> =
+        iolist_to_binary(Data),
+        {RCommand, RStatus, RId}
+    of
+        {8, Status, Id} ->
+            case lists:keytake(Id, 1, PendingList) of
+                false -> State;
+                {value,
+                 {_, {UserB, _, Token, DisableArgs} = Element}, NewPending} ->
+                    case Status of
+                        10 ->
+                            ?INFO_MSG("recoverable APNS error, retrying...", []),
+                            erlang:cancel_timer(RetryTimer),
+                            NewRetryTimer =
+                            erlang:send_after(?RETRY_INTERVAL, self(), retry),
+                            State#state{pending_list = NewPending,
+                                        retry_list = [Element|RetryList],
+                                        retry_timer = NewRetryTimer};
 
+                        7 ->
+                            ?INFO_MSG("recoverable APNS error, retrying...", []),
+                            erlang:cancel_timer(RetryTimer),
+                            NewRetryTimer =
+                            erlang:send_after(?RETRY_INTERVAL, self(), retry),
+                            State#state{
+                                pending_list = NewPending,
+                                retry_list =
+                                [{UserB, [], Token, DisableArgs}|RetryList],
+                                retry_timer = NewRetryTimer};
+
+                        S ->
+                            ?INFO_MSG("non-recoverable APNS error: ~p", [S]),
+                            mod_push:unregister_client(DisableArgs),
+                            State#state{pending_list = NewPending}
+                    end
+            end;
+
+        _ ->
+            ?ERROR_MSG("invalid APNS response", []),
+            State
+    catch {'EXIT', _} ->
+        ?ERROR_MSG("invalud APNS response", []),
+        State
+    end,
+    self() ! send,
+    {noreply, NewState#state{pending_timestamp = undefined}};
+         
 handle_info({ssl_closed, _SslSocket}, State) ->
     ?INFO_MSG("connection to APNS closed!", []),
     {noreply, State#state{out_socket = undefined}};
+
+%% retry_timeout
+handle_info(retry, #state{send_list = SendList,
+                          retry_list = RetryList,
+                          pending_timer = PendingTimer} = State) ->
+    NewSendList = SendList ++ RetryList,
+    case erlang:read_timer(PendingTimer) of
+        false -> self() ! send;
+        _ -> ok
+    end,
+    {noreply, State#state{send_list = NewSendList, retry_list = []}};
+
+%% pending_timeout
+handle_info({pending_timeout, Timestamp},
+            #state{pending_timestamp = StoredTimestamp} = State) ->
+    NewState =
+    case Timestamp of
+        StoredTimestamp ->
+            self() ! send,
+            State#state{pending_list = []};
+
+        _ -> State
+    end,
+    {noreply, NewState};
+
+handle_info(send, #state{certfile = CertFile,
+                         out_socket = OldSocket,
+                         pending_list = PendingList,
+                         send_list = SendList,
+                         retry_list = RetryList,
+                         retry_timer = RetryTimer,
+                         message_id = MessageId} = State) ->
+    NewState =
+    case get_socket(OldSocket, CertFile) of
+        {error, Reason} ->
+            ?ERROR_MSG("connection to APNS failed: ~p", [Reason]),
+            NewRetryList =        
+            pending_to_retry(PendingList, RetryList),
+            erlang:cancel_timer(RetryTimer),
+            NewRetryTimer =
+            erlang:send_after(?RETRY_INTERVAL, self(), retry),
+            State#state{out_socket = error,
+                        pending_list = [],
+                        retry_list = NewRetryList,
+                        retry_timer = NewRetryTimer};
+
+        Socket ->
+            PendingSpace = ?MAX_PENDING_NOTIFICATIONS - length(PendingList),
+            {NewPendingElements, NewSendList} =
+            case length(SendList) > PendingSpace of
+                true -> lists:split(PendingSpace, SendList);
+                false -> {SendList, []}
+            end,
+            MakeMessageId =
+            fun 
+                (Max) when Max =:= (4294967296 - 1) -> 0;
+                (OldMessageId) -> OldMessageId + 1
+            end,
+            {NewMessageId, Result} =
+            lists:foldl(
+                fun(Element, {MessageIdAcc, PendingListAcc}) ->
+                    NewId = MakeMessageId(MessageIdAcc),
+                    {NewId, [{NewId, Element}|PendingListAcc]}
+                end,
+                {MessageId, []},
+                NewPendingElements),
+            NewPendingList = PendingList ++ Result,
+            case NewPendingList of
+                [] ->
+                    State#state{pending_list = NewPendingList,
+                                send_list = NewSendList,
+                                message_id = NewMessageId};
+
+                _ ->
+                    Notifications =
+                    make_notifications(NewPendingList),
+                    case ssl:send(Socket, Notifications) of
+                        {error, Reason} ->
+                            ?ERROR_MSG("sending to APNS failed: ~p",
+                                       [Reason]),
+                            NewRetryList =
+                            pending_to_retry(NewPendingList, RetryList),
+                            erlang:cancel_timer(RetryTimer),
+                            NewRetryTimer =
+                            erlang:send_after(?RETRY_INTERVAL, self(), retry),
+                            State#state{out_socket = error,
+                                        pending_list = [],
+                                        retry_list = NewRetryList,
+                                        send_list = NewSendList,
+                                        retry_timer = NewRetryTimer,
+                                        message_id = NewMessageId};
+
+                        ok ->
+                            ?INFO_MSG("sending to APNS successful", []),
+                            Timestamp = erlang:now(),
+                            NewPendingTimer =
+                            erlang:send_after(?PENDING_INTERVAL, self(),
+                                              {pending_timeout, Timestamp}),
+                            State#state{out_socket = Socket,
+                                        pending_list = NewPendingList,
+                                        send_list = NewSendList,
+                                        pending_timer = NewPendingTimer,
+                                        message_id = NewMessageId,
+                                        pending_timestamp = Timestamp}
+                    end
+            end
+    end,
+    {noreply, NewState};
 
 handle_info(Info, State) ->
     ?DEBUG("+++++++ mod_push_apns received unexpected signal ~p", [Info]),
@@ -83,58 +258,24 @@ handle_info(Info, State) ->
 
 %-------------------------------------------------------------------------
 
-handle_call(_Req, _From, State) -> {noreply, State}.
+handle_call(_Req, _From, State) -> {reply, {error, badarg}, State}.
 
 %-------------------------------------------------------------------------
 
-%% TODO: store {MessageId, DisableCb} tuples and handle invalid-token
-%% error messages
-handle_cast({dispatch, _UserBare, Payload, Token, _AppId, DisableArgs},
-            #state{certfile = CertFile, out_socket = OutSocket} = State) ->
+%% new message
+handle_cast({dispatch, UserBare, Payload, Token, _AppId, DisableArgs},
+            #state{send_list = SendList,
+                   pending_timer = PendingTimer,
+                   retry_timer = RetryTimer} = State) ->
     ?DEBUG("+++++ Sending push notification to ~p", [?PUSH_URL]),
-    Socket = case OutSocket of
-        undefined ->
-            ?DEBUG("++++++ no connection found, connecting...", []),
-            SslOpts =
-            [{certfile, CertFile}, {versions, ['tlsv1.2']}, {ciphers, ?CIPHERSUITES}],
-            case ssl:connect(?PUSH_URL, ?APNS_PORT, SslOpts, ?SSL_TIMEOUT) of
-                {ok, S} -> S;
-                {error, E} -> {error, E}
-            end;
-
-        _ -> OutSocket
+    NewSendList =
+    lists:keystore(UserBare, 1, SendList,
+                   {UserBare, Payload, Token, DisableArgs}),
+    case {erlang:read_timer(PendingTimer), erlang:read_timer(RetryTimer)} of
+        {false, false} -> self() ! send;
+        _ -> ok
     end,
-    PushMessage =
-    {struct,
-     [{aps, {struct, [{'content-available', 1}|Payload]}}]},
-    EncodedMessage =
-    iolist_to_binary(mochijson2:encode(PushMessage)),
-    ?DEBUG("++++++ Encoded message: ~p", [EncodedMessage]),
-    MessageLength = size(EncodedMessage),
-    MessageId = randoms:get_string(),
-    TokenLength = size(Token),
-    ?DEBUG("Token: ~p, TokenLength: ~p", [Token, TokenLength]),
-    case Socket of
-        {error, Reason} ->
-            ?INFO_MSG("Could not connect to APNS: ~p", [Reason]),
-            {noreply, State#state{out_socket = undefined}};
-
-        _ ->
-            %% FIXME: MessageLength field has variable length, its value must be < 2K
-            Frame =
-             <<1:8, TokenLength:16/big, Token/binary,
-               2:8, MessageLength:16/big, EncodedMessage/binary>>,
-               %3:8, 4:16/big, MessageId/binary,
-               %4:8, 4:16/big, ?MESSAGE_EXPIRY_TIME:4/big-unsigned-integer-unit:8,
-               %5:8, 1:16/big, ?MESSAGE_PRIORITY:8>>,
-            FrameLength = size(Frame),
-            Packet =
-            <<2:8, FrameLength:32/big, Frame/binary>>,
-            ?DEBUG("++++++ Packet: ~p", [Packet]),
-            SendResult = ssl:send(Socket, Packet),
-            ?DEBUG("+++++ ssl:send returned ~p", [SendResult]),
-            {noreply, State#state{out_socket = Socket}}
-    end;
+    {noreply, State#state{send_list = NewSendList}};
 
 handle_cast(_Req, State) -> {reply, {error, badarg}, State}.
 
@@ -143,11 +284,70 @@ handle_cast(_Req, State) -> {reply, {error, badarg}, State}.
 terminate(_Reason, #state{out_socket = OutSocket}) ->
     case OutSocket of
         undefined -> ok;
+        error -> ok;
         _ -> ssl:close(OutSocket)
-    end.
+    end,
+    ok.
 
 %-------------------------------------------------------------------------
 
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+%-------------------------------------------------------------------------
+
+make_notifications(PendingList) ->
+    lists:foldl(
+        fun({MessageId, {_, Payload, Token, _}}, Acc) ->
+            PushMessage =
+            {struct,
+             [{aps, {struct, [{'content-available', 1}|Payload]}}]},
+            EncodedMessage =
+            iolist_to_binary(mochijson2:encode(PushMessage)),
+            ?DEBUG("++++++ Encoded message: ~p", [EncodedMessage]),
+            MessageLength = size(EncodedMessage),
+            TokenLength = size(Token),
+            ?DEBUG("Token: ~p, TokenLength: ~p", [Token, TokenLength]),
+            Frame =
+             <<1:1/unit:8, TokenLength:2/unit:8, Token/binary,
+               2:1/unit:8, MessageLength:2/unit:8, EncodedMessage/binary,
+               3:1/unit:8, 4:2/unit:8, MessageId:4/unit:8>>,
+               %4:8, 4:16/big, ?MESSAGE_EXPIRY_TIME:4/big-unsigned-integer-unit:8,
+               %5:8, 1:16/big, ?MESSAGE_PRIORITY:8>>,
+            FrameLength = size(Frame),
+            <<<<2:1/unit:8, FrameLength:4/unit:8, Frame/binary>>/binary, Acc/binary>>
+        end,
+        <<"">>,
+        PendingList).
+
+%-------------------------------------------------------------------------
+
+get_socket(OldSocket, CertFile) ->
+    case OldSocket of
+        _Invalid when OldSocket =:= undefined;
+                     OldSocket =:= error ->
+            SslOpts =
+            [{certfile, CertFile},
+             {versions, ['tlsv1.2']},
+             {ciphers, ?CIPHERSUITES},
+             {reuse_sessions, true},
+             {secure_renegotiate, true}],
+             %{verify, verify_peer},
+             %{cacertfile, CACertFile}],
+            case ssl:connect(?PUSH_URL, ?APNS_PORT, SslOpts, ?SSL_TIMEOUT) of
+                {ok, S} -> S;
+                {error, E} -> {error, E}
+            end;
+
+       _ -> OldSocket
+    end.
+
+%-------------------------------------------------------------------------
+
+pending_to_retry(PendingList, RetryList) ->
+    %% FIXME: keystore
+    RetryList ++
+    lists:map(
+        fun({_, Element}) -> Element end,
+        PendingList).
 
 %-------------------------------------------------------------------------
